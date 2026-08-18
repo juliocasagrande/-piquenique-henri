8ed98bb64b9ea6234093b6099af51d60e9382903
juliocasagrande <111095354+juliocasagrande@users.noreply.github.com>
2026-08-17 23:02:01 -0400

    Atualiza endpoint do Google Apps Script

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Endpoint publicado do Google Apps Script responsável por salvar as confirmações na planilha.
 window.RSVP_CONFIG = {
-  endpoint: "https://script.google.com/macros/s/AKfycby6YpzdD4kJpLVy5sxR_zY9UK8eQtQdzEx8UHzeZJQGNCBVea3wu5LtYoX1bH1K3MkUAg/exec",
+  endpoint: "https://script.google.com/macros/s/AKfycbwv_VBfxT5Q2Yvxv0xO-iTUy58arZ5DRfluA9C0JQxvKHaZhS-PipNSssCSgmP0vN46KQ/exec",
   eventDate: "2026-10-24T15:00:00-03:00"
 };
