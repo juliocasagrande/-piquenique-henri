16d6a64cae07e8bf1132c3a0a84eac9510885954
juliocasagrande <111095354+juliocasagrande@users.noreply.github.com>
2026-08-17 22:49:48 -0300

    Adiciona configuração da integração com a planilha

---
+++ config.js
@@ -0,0 +1,6 @@
+// Após publicar o Google Apps Script como Aplicativo da Web,
+// cole abaixo a URL que termina em /exec.
+window.RSVP_CONFIG = {
+  endpoint: "COLE_AQUI_A_URL_DO_GOOGLE_APPS_SCRIPT",
+  eventDate: "2026-10-24T15:00:00-03:00"
+};
