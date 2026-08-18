7d4224f5ec58434286a9506545cec0848ecda406
juliocasagrande <111095354+juliocasagrande@users.noreply.github.com>
2026-08-18 07:03:23 -0400

    Atualiza URL da implantação do Google Apps Script

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Endpoint publicado do Google Apps Script responsável por salvar as confirmações na planilha.
 window.RSVP_CONFIG = {
-  endpoint: "https://script.google.com/macros/s/AKfycbwv_VBfxT5Q2Yvxv0xO-iTUy58arZ5DRfluA9C0JQxvKHaZhS-PipNSssCSgmP0vN46KQ/exec",
+  endpoint: "https://script.google.com/macros/s/AKfycbzEP0u-coSHHb-FpyMgIGzNa_w4AiTZ_fi0IaMEElMHZpGnTLOv3sDAJc7xXjlhUmJf/exec",
   eventDate: "2026-10-24T15:00:00-03:00"
 };
